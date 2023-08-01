05c25b14f9cff211b878ab9d9532463bcafeb3e3
michioxd <michio.haiyaku@gmail.com>
2023-08-01 20:29:32 +0700

    lmao

--- src/command.ts
+++ src/command.ts
@@ -4,13 +4,15 @@ import demo from './commands/demo.js';
 import bonk from './commands/bonk.js';
 
 import admin_createNotify from './commands/admin_createNotify.js';
+import admin_message from './commands/admin_message.js';
 
 
 const cmds: CommandType[] = [
     demo,
     //@ts-ignore
     bonk,
-    admin_createNotify
+    admin_createNotify,
+    admin_message
 ];
 
 export default cmds;
