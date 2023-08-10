497f01eb2b6ad629f524ec5527d6f9d377345bc0
michioxd <michio.haiyaku@gmail.com>
2023-08-10 20:33:03 +0700

    lol

--- src/events/simbot.ts
+++ src/events/simbot.ts
@@ -18,13 +18,13 @@ const evt = {
 
         if (ct.content === "help" || ct.content === "giup" || ct.content === "deo biet dung") {
             //@ts-ignore
-            channel.send("Cách dùng `. <nội dung cần nhắn>`\nVí dụ: `. đm m nqu the =))`");
+            channel.send("Cách dùng `.<nội dung cần nhắn>` \nVí dụ: `.đm m nqu the =))`");
             return;
         }
 
         if (!ct.content.startsWith(".")) return;
 
-        const msg = ct.content.slice(2, ct.content.length);
+        const msg = ct.content.slice(1, ct.content.length);
         //const msg = ct.content;
 
         if (msg.length < 1) return;
