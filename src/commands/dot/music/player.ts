49ecfba254d343832bfa0082abdaa204f20ccab0
michioxd <michio.haiyaku@gmail.com>
2023-11-17 22:47:35 +0700

    a

--- src/commands/dot/music/player.ts
+++ src/commands/dot/music/player.ts
@@ -72,8 +72,7 @@ export async function HandlePlayingSession(type?: number) {
                         "user-agent": "Mozilla/ 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 119.0.0.0 Safari / 537.36",
                         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                         "Sec-Ch-Ua-Platform": "Windows"
-                    },
-                    retry: 1
+                    }
                 }), async function () {
                     //@ts-ignore
                     await client.guilds.cache.get(serverId).channels.cache.get(CurrentVoiceChannelId).send("❌ Đã xảy ra lỗi trong khi phát bài hát này, đang chuyển qua bài khác...");
