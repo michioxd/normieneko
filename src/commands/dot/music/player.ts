c7a3204519e3c9dca8b9a17dd157bafbbbbfc841
michioxd <michio.haiyaku@gmail.com>
2023-11-17 22:33:13 +0700

    h2

--- src/commands/dot/music/player.ts
+++ src/commands/dot/music/player.ts
@@ -73,7 +73,9 @@ export async function HandlePlayingSession(type?: number) {
                         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                         "Sec-Ch-Ua-Platform": "Windows"
                     },
-                    minVersion: "TLSv1.2",
+
+                    //@ts-ignore
+                    http2: true
 
                 }), async function () {
                     //@ts-ignore
