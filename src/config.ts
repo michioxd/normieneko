7e88ef98bc1e96fea770b7d7a9d02dde03233126
michioxd <michio.haiyaku@gmail.com>
2024-01-01 23:20:24 +0700

    update endpoint

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@
 const cfg = {
     globalPrefix: ";",
     serverId: "1126875840936955934",
-    invidiousEndpoint: "vid.priv.au",
+    invidiousEndpoint: "vid.puffyan.us",
     spotifyApi: {
         cid: "e744494ebfa7403b83048b91ec0909ef",
         cs: "4a8fd895300a4e58b89369df30e030db"
