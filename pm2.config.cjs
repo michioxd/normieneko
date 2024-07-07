83fa9c2fb8d55f522aa7f7093efc0ba51f591de7
michioxd <michio.haiyaku@gmail.com>
2024-07-07 16:52:20 +0700

    lmao

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   name: "normieneko", // Name of your application
   script: "./src/index.ts", // Entry point of your application
-  interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
+  interpreter: "bun", // Path to the Bun interpreter
 };
