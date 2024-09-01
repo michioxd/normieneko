aa8b6d19c55875d5a171db19ea85dc253ec2a298
michioxd <80969068+michioxd@users.noreply.github.com>
2024-09-02 01:19:58 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,8 @@ const cfg = {
     serverId: "1126875840936955934",
     invidiousEndpoint: "vid.puffyan.us",
     spotifyApi: {
-        cid: "e744494ebfa7403b83048b91ec0909ef",
-        cs: "4a8fd895300a4e58b89369df30e030db"
+        cid: "spotify cid",
+        cs: "Spotify secret"
     }
 };
 
